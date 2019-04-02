3927d7c9a2913c5241d4e7914010e5af58858050
DanielaFBL <48950358+DanielaFBL@users.noreply.github.com>
2019-04-02 07:59:27 -0500

    Add files via upload

---
+++ marcha.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <random>
+#include <stdlib.h>
+
+int main(){
+  int i;
+  
+  float x=0;
+  float y=0;
+  float dx;
+  float dy;
+  float pi = 3.14159265359;
+  float angulo;
+  srand48(42);
+
+  for (i=0;i<1000;i++){ 
+      
+    std::cout << x << " " << y << std::endl;
+      
+    angulo = drand48()*2*pi;
+    dx= x + cos(angulo);
+       x = dx;
+    dy= y + sin(angulo);
+       y = dy;
+    
+    
+    }
+ 
+  
+  return 0;
+}
